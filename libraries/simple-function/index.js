12f66fcc3349f54edf83fa0072ff8964310d0c0d
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2024-10-24 10:58:42 +0700

    add fetch js content

--- libraries/simple-function/index.js
+++ libraries/simple-function/index.js
@@ -1,6 +1,21 @@
 export default {
-	add: (a, b) => {
-		return a+b
-	},
-	
-}
+    /**
+     * This module provides functionality to try custom lib.
+     * @module JSSimpleFunction
+     */
+
+    /**
+     * Asynchronously fetches JavaScript content from a URL.
+     * @function fetchJsContent
+     * @param {string} url - The URL to fetch the JavaScript content from.
+     * @returns {Promise<string>} A promise that resolves with the fetched JavaScript content as text.
+     * @async
+     */
+    async fetchJsContent(url) {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch JS content from ${url}`);
+        }
+        return response.text();
+    },
+};
