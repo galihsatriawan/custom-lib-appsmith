a9fe2c1264858919cfcd38fd1deb21bd69c955ac
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2024-10-31 07:08:43 +0700

    update lib version

--- rollup.config.js
+++ rollup.config.js
@@ -5,38 +5,33 @@ import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
 import fs from 'fs';
 import replace from '@rollup/plugin-replace';
-
-const baseDir = path.resolve(__dirname, '.');
-
-// Dynamically generate the Rollup configuration for each library
-const libraryFolders = fs.readdirSync(path.join(baseDir, 'libraries')).filter(file => {
-  return fs.statSync(path.join(baseDir, 'libraries', file)).isDirectory();
+// Dynamically generate the input configuration for Rollup.
+// This is going to parse the folders in the `libraries` directory.
+const libraryFolders = fs.readdirSync('libraries').filter(function (file) {
+  return fs.statSync(path.join('libraries', file)).isDirectory();
 });
 
-const outputConfig = libraryFolders.map(folder => {
-  return {
-    input: path.join(baseDir, `libraries/${folder}/index.js`),
-    output: {
-      file: path.join(baseDir, `dist/${folder}.umd.js`),
-      format: 'umd',
-      name: folder.replace(/-\w/g, m => m[1].toUpperCase()), // Convert kebab-case to CamelCase
-      globals: {
-        // Define globals here if your libraries depend on external modules
-      }
-    },
-    plugins: [
-      resolve(),
-      terser(),
-      commonjs({
-        include: /node_modules/,
-      }),
-      replace({
-        preventAssignment: true
-      }),
-    ],
-    external: [
-      // Add external dependencies here if needed
-    ]
-  };
-});
+// We expect that each library will have a single `index.js` file that will be processed.
+const inputConfig = libraryFolders.map(folder => `libraries/${folder}/index.js`);
+
+// Generate output configuration for each library
+const outputConfig = libraryFolders.map(folder => ({
+  input: `libraries/${folder}/index.js`,
+  output: {
+    file: `dist/${folder}.umd.js`,
+    format: 'umd',
+    name: folder.replace(/-\w/g, m => m[1].toUpperCase()), // Convert kebab-case to CamelCase
+  },
+  plugins: [
+    resolve(),
+    terser(),
+    commonjs({
+      include: /node_modules/,
+    }),
+    replace({
+      preventAssignment: true,
+    }),
+  ]
+}));
+
 export default outputConfig;
